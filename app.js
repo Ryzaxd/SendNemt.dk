7edf4390c316b2137d611f1c4229809679ea9256
Ryzaxd <nick986h@edu.zealand.dk>
2024-06-13 23:59:30 +0200

    Revert "hosted på azure nu"
    This reverts commit 7354ec52372618768c0709c5ef2c14163f9dba61.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(session({
   secret: 'your-random-secret-key', 
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
+  cookie: { secure: false }
 }));
 
 app.use(logger('dev'));
