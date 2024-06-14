a8f10e247871c7048d7703ee78bc234f6a2ce8c2
Ryzaxd <nick986h@edu.zealand.dk>
2024-06-14 16:57:09 +0200

    session fejl med login

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const db = require('./models'); 
+const bcrypt = require('bcrypt');
+const indexRouter = require('./routes/index');
 
-var indexRouter = require('./routes/index');
+const app = express();
 
-var app = express();
+// Initialize SequelizeStore with the sequelize instance
+const sessionStore = new SequelizeStore({
+  db: db.sequelize, // Ensure sequelize is correctly referenced from db
+});
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
-app.use(session({
-  secret: 'your-random-secret-key', 
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.set("trust proxy", 1);
 
+// Session setup
+app.use(
+  session({
+    secret: 'your-random-secret-key', 
+    store: sessionStore,
+    resave: false,
+    proxy: true,
+    saveUninitialized: true,
+    cookie: { secure: true, sameSite: "none" },
+  })
+);
+
+// Middleware setups
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Routes setup
 app.use('/', indexRouter);
 app.use('/formular', indexRouter);
 app.use('/sporPakke', indexRouter);
@@ -40,37 +55,36 @@ app.use('/sporetPakke', indexRouter);
 app.use('/tracePackage', indexRouter);
 app.use('/admin/login', indexRouter);
 
+// Sync session store
+sessionStore.sync();
 
-// catch 404 and forward to error handler
+// Sync database and start server
+db.sequelize.sync().then(() => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}).catch(error => {
+  console.error('Unable to connect to the database:', error);
+});
+
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // set title
+  // Set title
   res.locals.title = 'Error';
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
-// Graceful shutdown
-const gracefulShutdown = async () => {
-  console.log('Shutting down gracefully...');
-  await sequelize.close();
-  server.close(() => {
-    console.log('Closed out remaining connections');
-    process.exit(0);
-  });
-};
-
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGINT', gracefulShutdown);
 
 module.exports = app;
--- models/index.js
+++ models/index.js
@@ -21,10 +21,10 @@ fs
   });
 
   Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-      db[modelName].associate(db);
-    }
-  });
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
